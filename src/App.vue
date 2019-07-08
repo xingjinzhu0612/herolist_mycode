5cec6bd83631a3c17b2edc78e0bda2756dc8ea2b
xingjinzhu0612 <1018715459@qq.com>
2019-07-08 17:39:45 +0800

    实现AppSidebar

--- src/App.vue
+++ src/App.vue
@@ -4,25 +4,8 @@
     <app-header></app-header>
     <div class="container-fluid">
       <div class="row">
-        <div class="col-sm-3 col-md-2 sidebar">
-          <ul class="nav nav-sidebar">
-            <li class="active">
-              <a href="#">
-                Overview
-                <span class="sr-only">(current)</span>
-              </a>
-            </li>
-            <li>
-              <a href="#">Reports</a>
-            </li>
-            <li>
-              <a href="#">Analytics</a>
-            </li>
-            <li>
-              <a href="#">Export</a>
-            </li>
-          </ul>
-        </div>
+     <!-- AppSidebar -->
+     <app-sidebar></app-sidebar>
         <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
           <h2 class="sub-header">Hero List</h2>
           <a class="btn btn-success" href="add.html">Add</a>
@@ -274,12 +257,15 @@
 
 <script>
 
-  //导入AppHeader
+  //导入AppHeader组件
   import AppHeader from './components/AppHeader.vue'
+  //导入 AppSidebar 组件
+  import AppSidebar from './components/AppSidebar.vue'
 export default {
   name: "app",
   components: {
-    AppHeader
+    AppHeader,
+    AppSidebar
   }
 };
 </script>
