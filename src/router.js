4f203821a33ddcf66747114808616a16ca4d4a7f
xingjinzhu0612 <1018715459@qq.com>
2019-07-08 21:11:52 +0800

    添加英雄的路由配置

--- src/router.js
+++ src/router.js
@@ -9,6 +9,9 @@ Vue .use(VueRouter)
 import HeroList from './views/heroes/HeroList.vue'
 import WeaponList from './views/weapons/WeaponList.vue'
 import EquipList from './views/equips/EquipList.vue'
+
+//导入添加英雄的组件
+import HeroAdd from './views/heroes/HeroAdd.vue'
 //创建路由对象规则
 const router=new VueRouter({
   linkActiveClass:'active',
@@ -17,7 +20,9 @@ const router=new VueRouter({
     {name:'home',path:'/',redirect:'/heroes'},
     {name:'heroes',path:'/heroes',component:HeroList},
     {name:'weapons',path:'/weapons',component:WeaponList},
-    {name:'equipss',path:'/equips',component:EquipList}
+    {name:'equipss',path:'/equips',component:EquipList},
+    {name:'heroesadd',path:'/heroes/add',component:HeroAdd},
+
 
 
   ]
---
+++ src/views/heroes/HeroAdd.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+        <h2 class="sub-header">添加英雄</h2>
+        <form>
+          <div class="form-group">
+            <label for="txtName">英雄名称</label>
+            <input type="txt" class="form-control" id="txtName" placeholder="英雄名称">
+          </div>
+          <div class="form-group">
+            <label for="sex">英雄性别</label>
+            <select class="form-control">
+                <option value="男">男</option>
+                <option value="女">女</option>
+            </select>
+          </div>
+          <button type="submit" class="btn btn-success">提交</button>
+        </form>
+      </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
