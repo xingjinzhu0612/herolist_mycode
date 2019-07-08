0db66aab21dfb41bbf28bb61cdd25728bb8917fa
xingjinzhu0612 <1018715459@qq.com>
2019-07-08 17:52:37 +0800

    配置路由vue-router

--- src/main.js
+++ src/main.js
@@ -6,6 +6,19 @@ Vue.config.productionTip = false
 //导入样式文件
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import './assets/css/index.css'
+
+//导入vue-router
+import VueRouter from 'vue-router'
+//配置vue的插件 vue-router
+Vue .use(VueRouter)
+
+//创建路由对象规则
+const router=new VueRouter({
+  routes:[
+
+  ]
+})
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
