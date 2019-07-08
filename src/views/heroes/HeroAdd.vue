7be36560f7731932887de48a6132d5245178b0c0
xingjinzhu0612 <1018715459@qq.com>
2019-07-08 21:40:30 +0800

    实现添加工能

--- src/views/heroes/HeroAdd.vue
+++ src/views/heroes/HeroAdd.vue
@@ -4,23 +4,51 @@
         <form>
           <div class="form-group">
             <label for="txtName">英雄名称</label>
-            <input type="txt" class="form-control" id="txtName" placeholder="英雄名称">
+            <input type="txt" class="form-control" v-model=" formData.name" id="txtName" placeholder="英雄名称">
           </div>
           <div class="form-group">
             <label for="sex">英雄性别</label>
-            <select class="form-control">
+            <select class="form-control" v-model=" formData.gender">
                 <option value="男">男</option>
                 <option value="女">女</option>
             </select>
           </div>
-          <button type="submit" class="btn btn-success">提交</button>
+          <button  @click.prevent='add' class="btn btn-success">提交</button>
         </form>
       </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-
+    data(){
+        return{
+            //1.绑定文本框
+            formData:{
+                name:'',
+                gender:'男'
+            }
+        }
+    },
+    methods: {
+        //2.点击提交按钮,添加英雄
+        add(){
+            axios
+            .post('http://localhost:3000/heroes',this. formData)
+            .then((res)=>{
+                const status=res.status
+                if(status===201){
+                    //3跳转到列表组件
+                    this.$router.push({name:'heroes'})
+                }else{
+                    alert('添加失败')
+                }
+            })
+            .catch((err)=>{
+                alert('服务器错误'+err)
+            })
+        }
+    },
 }
 </script>
 
