2f32ddc543669b65b36610d45ba17c0a30ed118d
xingjinzhu0612 <1018715459@qq.com>
2019-07-08 20:50:46 +0800

    实现删除功能

--- src/views/heroes/HeroList.vue
+++ src/views/heroes/HeroList.vue
@@ -13,17 +13,16 @@
           </tr>
         </thead>
         <tbody>
-          <tr v-for='(item,index) in list' :key='item.id'>
+          <tr v-for="(item,index) in list" :key="item.id">
             <td>{{index+1}}</td>
             <td>{{item.name}}</td>
             <td>{{item.gender}}</td>
             <td>
-              <a href="edit.html">edit</a>
+              <a href="edit.html">编辑</a>
               &nbsp;&nbsp;
-              <a href="javascript:window.confirm('Are you sure?')">delete</a>
+              <a href="javascript:" @click="del(item.id)">删除</a>
             </td>
           </tr>
-      
         </tbody>
       </table>
     </div>
@@ -31,40 +30,61 @@
 </template>
 
 <script>
-
 //导入axios
-import axios from 'axios'
+import axios from "axios";
 export default {
-   data() {
+  data() {
     return {
       //存储英雄列表数据
-      list:[]
-    }
+      list: []
+    };
   },
   //组件加载完毕执行
   mounted() {
     //发送请求，获取数据
-    this.loadData()
+    this.loadData();
   },
   methods: {
-    loadData(){
+    loadData() {
       axios
-      .get('http://localhost:3000/heroes')
-      .then((res)=>{
-        console.log(res);
-        const {data,status}=res;
-        if(status===200){
-          this.list=data
-          console.log(this.list)
-        }else{
-          alert('获取数据失败')
-        }
-      })
-      .catch((err)=>{
-        console.log(err)
-      })
+        .get("http://localhost:3000/heroes")
+        .then(res => {
+          console.log(res);
+          const { data, status } = res;
+          if (status === 200) {
+            this.list = data;
+            console.log(this.list);
+          } else {
+            alert("获取数据失败");
+          }
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    },
+    //删除功能
+    del(id) {
+      //删除提示
+      if (!confirm("确认删除？")) {
+        return false;
+      }
+      axios
+        .delete(`http://localhost:3000/heroes/${id}`)
+        .then(res => {
+          console.log(res);
+          const status = res.status;
+          if (status === 200) {
+            //重新加载数据
+            this.loadData();
+          } else {
+            alert("删除失败");
+          }
+        })
+        .catch(err => {
+          alert("服务器错误" + err);
+        });
     }
-  },
+  }
 };
 </script>
 
