a57b8ddd1d6bacffe54e5aaa6952ab366db8ed17
xingjinzhu0612 <1018715459@qq.com>
2019-07-08 19:11:42 +0800

    实现herolist

--- src/components/AppSidebar.vue
+++ src/components/AppSidebar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="col-sm-3 col-md-2 sidebar">
     <ul class="nav nav-sidebar">
-      <li class="active">
+      <!-- <li class="active">
         <a href="#">
           英雄列表
           <span class="sr-only">(current)</span>
@@ -12,7 +12,16 @@
       </li>
       <li>
         <a href="#">装备列表</a>
-      </li>
+      </li> -->
+      <router-link class="active" to='/heroes' tag='li'>
+        <a>英雄列表</a>
+      </router-link>
+       <router-link to='/weapons' tag='li'>
+        <a>武器列表</a>
+      </router-link>
+       <router-link to='/equips' tag='li'>
+        <a>装备列表</a>
+      </router-link>
       
     </ul>
   </div>
