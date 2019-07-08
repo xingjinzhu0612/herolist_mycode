28c6890bf8cee0dba9747e3036faffced2f42ba2
xingjinzhu0612 <1018715459@qq.com>
2019-07-08 19:56:35 +0800

    提取路由模块

--- src/main.js
+++ src/main.js
@@ -7,29 +7,11 @@ Vue.config.productionTip = false
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import './assets/css/index.css'
 
-//导入vue-router
-import VueRouter from 'vue-router'
-//配置vue的插件 vue-router
-Vue .use(VueRouter)
+//导入路由模块
+import router from './router.js'
 
-//导入英雄列表的组件
-import HeroList from './views/heroes/HeroList.vue'
-import WeaponList from './views/weapons/WeaponList.vue'
-import EquipList from './views/equips/EquipList.vue'
-//创建路由对象规则
-const router=new VueRouter({
-  linkActiveClass:'active',
-  //配置路由规则
-  routes:[
-    {name:'home',path:'/',redirect:'/heroes'},
-    {name:'heroes',path:'/heroes',component:HeroList},
-    {name:'weapons',path:'/weapons',component:WeaponList},
-    {name:'equipss',path:'/equips',component:EquipList}
-
-
-  ]
-})
 new Vue({
   render: h => h(App),
+  //配置路由对象
   router
 }).$mount('#app')
